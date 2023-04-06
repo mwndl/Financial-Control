afe9a58ac066d41b44add67c56fc961f6afd5a39
Marcos Wiendl <marcos.wiendl@gmail.com>
2023-04-06 11:43:59 -0300

    Adição de favicon, implementação de página para integração com o banco de dados (apenas front) e correção do CSS no botão da navbar (visivel no mobile)

--- date-hour.js
+++
@@ -1,8 +0,0 @@
-var agora = new Date();
-var hora = agora.getHours();
-var minutos = agora.getMinutes();
-if (minutos < 10) {
-    minutos = "0" + minutos;
-}
-var valorPadrao = hora + ":" + minutos;
-document.getElementById("hora").value = valorPadrao;
